f566c660aa3a96f525a8fa6f00aeac756d2aad27
Jacob Doren <jakedoren@gmail.com>
2021-07-24 22:14:42 -0400

    conditional rendering for routers based on loggedin state

--- src/Router.js
+++ src/Router.js
@@ -1,17 +1,26 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Register from './components/auth/Register';
 import Login from './components/auth/Login';
+import AuthContext from './AuthContext/AuthContext';
 
 const Router = () => {
+
+    const { loggedin } = useContext(AuthContext);
+
     return <BrowserRouter>
         <Navbar />
         <Switch>
+
             <Route exact path="/" />
-            <Route path="/register" component={Register} />
-            <Route path="/login" component={Login} />
-            <Route path="/notes" />
+            {loggedin === false && (
+                <>
+                    <Route path="/register" component={Register} />
+                    <Route path="/login" component={Login} />
+                </>
+            )}
+            {loggedin === true && <Route path="/notes" />}
         </Switch>
     </BrowserRouter>
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,13 +1,27 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Link } from 'react-router-dom';
+import AuthContext from '../AuthContext/AuthContext';
 
 const Navbar = () => {
+
+    const { loggedin } = useContext(AuthContext);
+    console.log(loggedin)
+
     return (
+
         <div>
             <Link to="/">Home</Link>
-            <Link to="/register">Register</Link>
-            <Link to="/login">Log In</Link>
-            <Link to="/notes">Notes</Link>
+            {loggedin === false && (
+                <>
+                    <Link to="/register">Register</Link>
+                    <Link to="/login">Log In</Link>
+                </>
+            )}
+            {loggedin === true && (
+                <>
+                    <Link to="/notes">Notes</Link>
+                </>
+            )}
         </div>
     )
 };
